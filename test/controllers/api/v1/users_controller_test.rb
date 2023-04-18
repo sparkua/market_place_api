1cdb42874eadea1fb3fc549cf212de4b95f32082
Nick Logvynenko <mykola@luminary.audio>
2023-04-17 21:33:36 -0700

    Initial api application with basic JWT auth

---
+++ test/controllers/api/v1/users_controller_test.rb
@@ -0,0 +1,53 @@
+require "test_helper"
+
+class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do 
+      @user = users(:one) 
+  end
+
+  test "should show user" do 
+      get api_v1_user_url(@user), as: :json 
+      assert_response :success 
+      # Test to ensure response contains the correct email   
+      json_response = JSON.parse(self.response.body) 
+      assert_equal @user.email, json_response['email']   
+  end
+
+  test "should create user" do 
+    assert_difference('User.count') do 
+      post api_v1_users_url, params: { user: { email: 'test@test.org', password: '123456' } }, as: :json 
+    end 
+    assert_response :created 
+  end 
+  test "should not create user with taken email" do   
+    assert_no_difference('User.count') do 
+      post api_v1_users_url, params: { user: { email: @user .email, password: '123456' } }, as: :json 
+    end 
+  end
+
+  test "should update user" do 
+    patch api_v1_user_url(@user), 
+    params: { user: { email: @user.email } }, 
+    headers: { Authorization: JsonWebToken.encode(user_id: @user.id) }, as: :json 
+    assert_response :success 
+  end
+  test "should forbid update user" do 
+    patch api_v1_user_url(@user), params: { user: { email: @user.email } }, as: :json 
+    assert_response :forbidden 
+  end 
+
+  # DELETE /users/1
+  test "should destroy user" do
+    assert_difference('User.count', -1) do
+      delete api_v1_user_url(@user), headers: { Authorization:JsonWebToken.encode(user_id: @user.id) }, as: :json
+    end
+    assert_response :no_content
+  end
+  test "should forbid destroy user" do
+    assert_no_difference('User.count') do
+      delete api_v1_user_url(@user), as: :json
+    end
+    assert_response :forbidden
+  end
+
+end
